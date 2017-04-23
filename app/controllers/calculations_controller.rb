46e65b99ad67020bd3a01ba6a437b750a9158c98
Ai Nguyen <ainguyen01@users.noreply.github.com>
2017-04-22 22:54:19 -0500

    Save #1

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,56 @@
+class CalculationsController <ApplicationController
+
+  def square_results
+    @user_num = params["user_num"].to_f
+    render("calculations/square_results.html.erb")
+  end
+
+  def square_form
+    render("calculations/square_new.html.erb")
+  end
+
+  def flexible_square
+    @user_num = params[:number].to_f
+    render("calculations/flexible_square.html.erb")
+  end
+
+  def flexible_square_root
+    @user_num = params[:number].to_f
+    render("calculations/flexible_square_root.html.erb")
+  end
+
+  def square_root_results
+    @user_num = params["user_num"].to_f
+    render("calculations/square_root_results.html.erb")
+  end
+
+  def square_root_form
+    render("calculations/square_root_new.html.erb")
+  end
+
+  def monthly_payment
+    @user_term = params[:number_of_years].to_f
+    @user_rate = params[:basis_points].to_f
+    @user_loan = params[:present_value].to_f
+    @numerator=((@user_rate/120000)*@user_loan)
+    @period=(-@user_term*12)
+    @monthly_rate=(@user_rate/120000)
+    @payment= @numerator/(1-((1+@monthly_rate)**@period))
+    render ("calculations/flexible_payment.html.erb")
+  end
+
+def monthly_payment_results
+  @user_term = params["number_of_years"].to_f
+  @user_rate = params["basis_points"].to_f
+  @user_loan = params["present_value"].to_f
+  @numerator=((@user_rate/120000)*@user_loan)
+  @period=(-@user_term*12)
+  @monthly_rate=(@user_rate/120000)
+  @payment= @numerator/(1-((1+@monthly_rate)**@period))
+  render ("calculations/payment_results.html.erb")
+end
+
+def payment_form
+  render("calculations/payment_new.html.erb")
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get("flexible/square/:number", {:controller => "calculations", :action=> "flexible_square"})
+get("square/new", {:controller => "calculations", :action=> "square_form"})
+get("square/results", {:controller => "calculations", :action=> "square_results"})
+
+get("flexible/square_root/:number", {:controller => "calculations", :action=> "flexible_square_root"})
+get("square_root/new", {:controller => "calculations", :action=> "square_root_form"})
+get("square_root/results", {:controller => "calculations", :action=> "square_root_results"})
+
+get("flexible/payment/:basis_points/:number_of_years/:present_value", {:controller =>"calculations", :action => "monthly_payment"})
+get("payment/new", {:controller => "calculations", :action=> "payment_form"})
+get("payment/results", {:controller => "calculations", :action=> "payment_results"})
+
+
 end
